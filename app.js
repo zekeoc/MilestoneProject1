591066675bab6de263ba53bca7c865e2b883f323
zekeoc <79272827+zekeoc@users.noreply.github.com>
2022-09-04 12:16:14 -0700

    added cardlist, grid, card randomizer, and basic function for board

---
+++ app.js
@@ -0,0 +1,138 @@
+//reminder
+//element.addEventListener(event, function, useCapture);
+//load html content
+
+document.addEventListener('DOMContentLoaded', () => {
+
+ //introducing card set to call back on in future. referencing each card twice for matches 
+
+    const cardsList = [
+        {
+            name: 'blastoise',
+            image: 'cards.jpg/blastoise.jpg'
+        },
+        {
+            name: 'blastoise',
+            image: 'cards.jpg/blastoise.jpg'
+        },
+        {
+            name: 'charizard',
+            image: 'cards.jpg/charizard.jpg'
+        },
+        {
+            name: 'charizard',
+            image: 'cards.jpg/charizard.jpg'
+        },
+        {
+            name: 'groudon',
+            image: 'cards.jpg/groudon.jpg'
+        },
+        {
+            name: 'groudon',
+            image: 'cards.jpg/groudon.jpg'
+        },
+        {
+            name: 'mew',
+            image: 'cards.jpg/mew.jpg'
+        },
+        {
+            name: 'mew',
+            image: 'cards.jpg/mew.jpg'
+        },
+        {
+            name: 'mewtwo',
+            image: 'cards.jpg/mewtwo.jpg'
+        },
+        {
+            name: 'mewtwo',
+            image: 'cards.jpg/mewtwo.jpg'
+        },
+        {
+            name: 'oak',
+            image: 'cards.jpg/oak.jpg'
+        },
+        {
+            name: 'oak',
+            image: 'cards.jpg/oak.jpg'
+        },
+        {
+            name: 'pikachu',
+            image: 'cards.jpg/pikachu.jpg'
+        },
+        {
+            name: 'pikachu',
+            image: 'cards.jpg/pikachu.jpg'
+        },
+        {
+            name: 'teamrocket',
+            image: 'cards.jpg/teamrocket.jpg'
+        },
+        {
+            name: 'teamrocket',
+            image: 'cards.jpg/teamrocket.jpg'
+        },
+        {
+            name: 'umbreon',
+            image: 'cards.jpg/umbreon.jpg'
+        },
+        {
+            name: 'umbreon',
+            image: 'cards.jpg/umbreon.jpg'
+        },
+        {
+            name: 'venasaur',
+            image: 'cards.jpg/venasaure.jpg'
+        },
+        {
+            name: 'venasaur',
+            image: 'cards.jpg/venasaure.jpg'
+        },
+        {
+            name: 'blankcard',
+            image: 'cards.jpg/blankcard.png'
+        },
+        {
+            name: 'blankcard',
+            image: 'cards.jpg/blankcard.png'
+        },
+        {
+            name: 'placeholder',
+            image: 'cards.jpg/placeholder.jpg'
+        }
+    ]
+
+
+boop = cardsList.sort( () => 0.5 - Math.random() );
+
+//creating grid
+const grid = document.querySelector('.gameGrid')
+
+
+
+/* initiating board, func loops over all cards in li
+adds an image to all cards then appends to grid */
+
+function getBoard(){
+    for (var i =0; i< cardsList; i++){
+        var card = document.createElement('img');
+        card.setAttribute('src', 'cards.jpg/placeholder.jpg')
+        card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard)
+        grid.appendChild(card);
+    }
+}
+
+getBoard();
+
+
+console.log(boop)
+
+
+
+
+
+
+
+
+
+})
